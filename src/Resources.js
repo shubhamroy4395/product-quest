a123bab607e8343873c4c57d486f3a7addd748d8
shubhamroypm <shubhamroy4395@gmail.com>
2024-10-27 14:32:44 +0530

    Fixed build errors: Updated resource links and added Babel dependency

--- src/Resources.js
+++ src/Resources.js
@@ -12,19 +12,19 @@ function Resources() {
         <div className="resource-card">
           <h2>Product Strategy</h2>
           <p>Learn how to develop and execute winning product strategies</p>
-          <a href="#" className="resource-link">Explore Strategy Resources</a>
+          <a href="/strategy" className="resource-link">Explore Strategy Resources</a>
         </div>
         
         <div className="resource-card">
           <h2>User Research</h2>
           <p>Master the art of understanding user needs and behaviors</p>
-          <a href="#" className="resource-link">Explore Research Resources</a>
+          <a href="/research" className="resource-link">Explore Research Resources</a>
         </div>
         
         <div className="resource-card">
           <h2>Agile & Scrum</h2>
           <p>Deep dive into agile methodologies and scrum practices</p>
-          <a href="#" className="resource-link">Explore Agile Resources</a>
+          <a href="/agile" className="resource-link">Explore Agile Resources</a>
         </div>
       </section>
     </div>
